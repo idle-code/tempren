4c2810ea92b265bb89d5e2a21991a3339fa84ee5
Pawel Zukowski <p.z.idlecode@gmail.com>
2020-11-02 22:09:09 +0100

    Remove `Protocol` TagFactory dependency

--- tempren/template/tree_elements.py
+++ tempren/template/tree_elements.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import Any, Callable, List, Mapping, Optional, Protocol
+from typing import Any, Callable, List, Mapping, Optional
 
 
 class PatternElement(ABC):
