afc31858950bf1f00b75625e0e6bb27b52f09c22
Pawel Zukowski <p.z.idlecode@gmail.com>
2022-02-26 16:36:56 +0100

    Add TemplateFileFilter tests

--- tests/test_file_filters.py
+++ tests/test_file_filters.py
@@ -3,12 +3,17 @@ from pathlib import Path
 import pytest
 
 from tempren.file_filters import (
+    FileFilterInverter,
     GlobFilenameFileFilter,
     GlobPathFileFilter,
     RegexFilenameFileFilter,
     RegexPathFileFilter,
+    TemplateFileFilter,
 )
 from tempren.path_generator import File
+from tempren.template.tree_elements import Pattern, RawText, TagInstance
+
+from .template.mocks import MockTag
 
 
 class TestRegexFilenameFileFilter:
@@ -129,3 +134,57 @@ class TestGlobPathFileFilter:
 
         assert file_filter(File(Path("directory/file.foo")))
         assert file_filter(File(Path("DIRECTORY/file.foo"))) == ignore_case
+
+
+class TestFileFilterInverter:
+    @pytest.mark.parametrize("original_value", [False, True])
+    def test_original_value_is_negated(self, original_value: bool):
+        def original_filter(f: File) -> bool:
+            return original_value
+
+        inverted_filter = FileFilterInverter(original_filter)
+        file = File(Path("some/file.name"))
+
+        assert original_value == (not inverted_filter(file))
+
+
+class TestTemplateFileFilter:
+    @pytest.mark.parametrize("expression_text", ["", "$%", "1 +", "while True: pass"])
+    def test_invalid_expression(self, expression_text: str):
+        pattern = Pattern([RawText(expression_text)])
+        file_filter = TemplateFileFilter(pattern)
+        file = File(Path("some/file.name"))
+
+        with pytest.raises(SyntaxError):
+            file_filter(file)
+
+    @pytest.mark.parametrize(
+        "expression_text,expected_result",
+        [
+            ("True", True),
+            ("False", False),
+            ("1", True),
+            ("0", False),
+            ("''", False),
+            ("1 > 2", False),
+            ("1 < 2", True),
+            ("'PNG' == 'png' or len('PNG') == 3", True),
+        ],
+    )
+    def test_valid_expression(self, expression_text: str, expected_result: bool):
+        pattern = Pattern([RawText(expression_text)])
+        file_filter = TemplateFileFilter(pattern)
+        file = File(Path("some/file.name"))
+
+        assert file_filter(file) == expected_result
+
+    def test_file_is_passed_to_tags(self):
+        mock_tag = MockTag()
+        mock_tag.process_output = "True"
+        pattern = Pattern([TagInstance(tag=mock_tag)])
+        file_filter = TemplateFileFilter(pattern)
+        file_path = Path("some/file.name")
+
+        file_filter(File(file_path))
+
+        assert mock_tag.path == file_path
