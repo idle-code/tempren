9e30f29dda999f759700c78d5fd48323e43d8ce2
Pawel Zukowski <p.z.idlecode@gmail.com>
2022-07-22 17:11:29 +0200

    Add script for tagging releases

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+set -e
+BUMP_RULE=$1
+if [ -z "$BUMP_RULE" ]; then
+  echo "Please specify bump rule: major, minor or patch"
+  exit 1
+fi
+
+OLD_VERSION=$(poetry version --short)
+poetry version $BUMP_RULE
+NEW_VERSION=$(poetry version --short)
+
+git commit -a -m "Version v$NEW_VERSION"
+git tag "v$NEW_VERSION"
+
+echo "Use 'git push && git push --tags' to publish"
