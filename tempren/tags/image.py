c95c8cfa82f967e24e25ed458f9f92cad16b1c26
Pawel Zukowski <p.z.idlecode@gmail.com>
2022-07-25 19:57:57 +0200

    Add ExifTag image tag

--- tempren/tags/image.py
+++ tempren/tags/image.py
@@ -1,7 +1,11 @@
+import itertools
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Iterator, Optional, Tuple
 
+import piexif
 import PIL
+from piexif import TAGS
+from piexif import TYPES as TAG_TYPES
 from PIL import Image
 
 from tempren.path_generator import File
@@ -41,3 +45,66 @@ class HeightTag(PillowTagBase):
 
     def extract_metadata(self, image: Image) -> Any:
         return image.height
+
+
+class ExifTag(Tag):
+    """Extract value of any EXIF tag"""
+
+    tag_id: int
+    tag_type: int
+
+    def configure(self, tag_name: str):  # type: ignore
+        """
+        :param tag_name: name of the tag to extract (e.g. 'FocalLength', 'DateTime', 'FNumber')
+        """
+        # TODO: generate list of supported tags dynamically
+        assert tag_name, "expected non empty tag name"
+        self.tag_id, self.tag_type = self._tag_name_to_id_type(tag_name)
+
+    @staticmethod
+    def _tag_name_to_id_type(tag_name: str) -> Tuple[int, int]:
+        for _, tags in TAGS.items():
+            for tag_id, description in tags.items():
+                if description["name"] == tag_name:
+                    return tag_id, description["type"]
+        raise ValueError(f"Could not find tag id for '{tag_name}'")
+
+    def process(self, file: File, context: Optional[str]) -> Any:
+        exif_dict = piexif.load(str(file.absolute_path))
+        for src in exif_dict.values():
+            if isinstance(src, dict) and self.tag_id in src:
+                return self._extract_value(src[self.tag_id])
+        else:
+            raise MissingMetadataError()
+
+    def _extract_value(self, tag_value):
+        if self.tag_type in (TAG_TYPES.Rational, TAG_TYPES.SRational):
+            return round(tag_value[0] / tag_value[1], 1)
+        if self.tag_type == TAG_TYPES.Ascii:
+            return tag_value.decode("ascii")
+        return tag_value
+
+
+_tag_type_map = {
+    TAG_TYPES.__dict__[type_name]: type_name
+    for type_name in TAG_TYPES.__dict__.keys()
+    if isinstance(TAG_TYPES.__dict__[type_name], int)
+}
+
+
+def _generate_exif_tag_list() -> Iterator[str]:
+    for _, tags in TAGS.items():
+        for tag_id, description in tags.items():
+            tag_name = description["name"]
+            if tag_name.startswith("ZZZTest"):
+                continue
+            tag_type = _tag_type_map[description["type"]]
+            yield f"  {tag_name} ({tag_type})"
+
+
+ExifTag.__doc__ = "\n".join(
+    itertools.chain(
+        [str(ExifTag.__doc__), "Available tag names:"],
+        sorted(set(_generate_exif_tag_list())),
+    )
+)
--- tests/tags/test_image.py
+++ tests/tags/test_image.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 from tempren.path_generator import File
-from tempren.tags.image import HeightTag, WidthTag
+from tempren.tags.image import ExifTag, HeightTag, WidthTag
 from tempren.template.tree_elements import FileNotSupportedError, MissingMetadataError
 
 
@@ -55,3 +55,61 @@ class TestHeightTag:
 
         with pytest.raises(FileNotSupportedError):
             tag.process(text_file, None)
+
+
+class TestExifTag:
+    def test_no_tag_name_provided(self, image_data_dir: Path):
+        tag = ExifTag()
+
+        with pytest.raises(AssertionError):
+            tag.configure("")
+
+    def test_unknown_tag_name(self, image_data_dir: Path):
+        tag = ExifTag()
+
+        with pytest.raises(ValueError):
+            tag.configure("Unknown tag name")
+
+    def test_missing_tag_value(self, image_data_dir: Path):
+        tag = ExifTag()
+        tag.configure("BitsPerSample")
+        jpeg_file = File(image_data_dir, Path("photo.jpg"))
+
+        with pytest.raises(MissingMetadataError):
+            tag.process(jpeg_file, None)
+
+    def test_date_time(self, image_data_dir: Path):
+        tag = ExifTag()
+        tag.configure("DateTime")
+        jpeg_file = File(image_data_dir, Path("photo.jpg"))
+
+        gps_date_stamp = tag.process(jpeg_file, None)
+
+        assert gps_date_stamp == "2019:02:17 07:20:40"
+
+    def test_gps_date_stamp_ascii(self, image_data_dir: Path):
+        tag = ExifTag()
+        tag.configure("GPSDateStamp")
+        jpeg_file = File(image_data_dir, Path("photo.jpg"))
+
+        gps_date_stamp = tag.process(jpeg_file, None)
+
+        assert gps_date_stamp == "2019:02:17"
+
+    def test_focal_length_rational(self, image_data_dir: Path):
+        tag = ExifTag()
+        tag.configure("FocalLength")
+        jpeg_file = File(image_data_dir, Path("photo.jpg"))
+
+        focal_length = tag.process(jpeg_file, None)
+
+        assert round(focal_length, 1) == 4.2
+
+    def test_focal_length_mm_short(self, image_data_dir: Path):
+        tag = ExifTag()
+        tag.configure("FocalLengthIn35mmFilm")
+        jpeg_file = File(image_data_dir, Path("photo.jpg"))
+
+        focal_length_mm = tag.process(jpeg_file, None)
+
+        assert focal_length_mm == 26
